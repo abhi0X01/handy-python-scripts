634e3729298aa6b6c4a4d4dd17e60cac2d793298
quicksilverOXO1 <quicksilver0x01@gmail.com>
2022-04-03 01:34:03 -0400

    fixed python indentation errors

--- split-base64.py
+++ split-base64.py
@@ -4,4 +4,4 @@
 str = "powershell.exe -nop -w hidden -e JABzACAAPQAgAE4AZQB3ACadhoasdfasdoffhfoahfaosfhaohfasoifhasoihaosidfah"
 n = 50
 for i in range(0, len(str), n):
-print "Str = Str + " + '"' + str[i:i+n] + '"'
+    print ("Str = Str + " + '"' + str[i:i+n] + '"')
